7520a270b3fb95ba0f3f54e42107cfcc9b66a416
Steve Bishop <ringersring@hotmail.com>
2019-10-21 15:04:27 -0700

    Added Relationships and migrations

---
+++ Migrations/20191021212646_Relationships.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPIStarter.Migrations
+{
+    public partial class Relationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AddressTypeId",
+                table: "Addresses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "AddressTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AddressTypeName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AddressTypes", x => x.Id);
+                });
+            if (ActiveProvider == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                migrationBuilder.DropTable(name: "Addresses");
+                migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Line1 = table.Column<string>(nullable: true),
+                    Line2 = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    StateProvince = table.Column<string>(nullable: true),
+                    Zipcode = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    AddressTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_AddressTypes_AddressTypeId",
+                        column: x => x.AddressTypeId,
+                        principalTable: "AddressTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+            }
+            else
+            {
+                migrationBuilder.AlterColumn<string>(
+                    name: "Line1",
+                    table: "Addresses",
+                    maxLength: 100,
+                    nullable: false,
+                    oldClrType: typeof(string),
+                    oldType: "TEXT",
+                    oldNullable: true);
+            }
+            migrationBuilder.CreateTable(
+                name: "CustomerAddress",
+                columns: table => new
+                {
+                    CustomerId = table.Column<long>(nullable: false),
+                    AddressId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerAddress", x => new { x.CustomerId, x.AddressId });
+                    table.ForeignKey(
+                        name: "FK_CustomerAddress_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CustomerAddress_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_AddressTypeId",
+                table: "Addresses",
+                column: "AddressTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAddress_AddressId",
+                table: "CustomerAddress",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_AddressTypes_AddressTypeId",
+                table: "Addresses",
+                column: "AddressTypeId",
+                principalTable: "AddressTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);\
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_AddressTypes_AddressTypeId",
+                table: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "AddressTypes");
+
+            migrationBuilder.DropTable(
+                name: "CustomerAddress");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_AddressTypeId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "AddressTypeId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Line1",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
